cc4e125d315bd113f97f7877bf1690cbd60b69ab
joaquin <jgamboa007@gmail.com>
2018-10-15 15:34:50 -0300

    Primer commit

---
+++ ApiCEM/Controllers/NotasController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using ApiCEM.Models;
+
+namespace ApiCEM.Controllers
+{
+    public class NotasController : ApiController
+    {
+        private CEMRESTApiEntities db = new CEMRESTApiEntities();
+
+        // GET: api/Notas
+        public IQueryable<Notas> GetNotas()
+        {
+            return db.Notas;
+        }
+
+        // GET: api/Notas/5
+        [ResponseType(typeof(Notas))]
+        public IHttpActionResult GetNotas(int id)
+        {
+            Notas notas = db.Notas.Find(id);
+            if (notas == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(notas);
+        }
+
+        // PUT: api/Notas/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutNotas(int id, Notas notas)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != notas.idNota)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(notas).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!NotasExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Notas
+        [ResponseType(typeof(Notas))]
+        public IHttpActionResult PostNotas(Notas notas)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Notas.Add(notas);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = notas.idNota }, notas);
+        }
+
+        // DELETE: api/Notas/5
+        [ResponseType(typeof(Notas))]
+        public IHttpActionResult DeleteNotas(int id)
+        {
+            Notas notas = db.Notas.Find(id);
+            if (notas == null)
+            {
+                return NotFound();
+            }
+
+            db.Notas.Remove(notas);
+            db.SaveChanges();
+
+            return Ok(notas);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool NotasExists(int id)
+        {
+            return db.Notas.Count(e => e.idNota == id) > 0;
+        }
+    }
+}
---
+++ ApiCEM/obj/Release/Package/PackageTmp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ApiCEM.MvcApplication" Language="C#" %>
